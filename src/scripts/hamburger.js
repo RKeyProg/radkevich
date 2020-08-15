1309fab3530e30786a06be57a40e32e39ca4fac1
Kirill Radkevich <rkey_prog@mail.ru>
2020-08-16 00:31:00 +0300

    create modile-menu. Add script on hamburger. Change index.pug struct

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,10 +1,21 @@
 import Vue from "vue";
 
 new Vue({
-    el: '#hamburger',
+    el: '#welcome-section',
+    data: {
+        styles: {
+            right: '-9999px',
+            opacity: 0
+        }
+    },
     methods: {
         viewMobileMenu() {
-            
+            this.styles.right = 0;
+            this.styles.opacity = 1;
+        },
+        hideMobileMenu() {
+            this.styles.right = '-9999px';
+            this.styles.opacity = 0;
         }
     }
 })
