e7cae85e235f62c1157305b2e2e12e55a2338eab
Kirill Radkevich <rkey_prog@mail.ru>
2020-08-21 01:26:08 +0300

    create on admin: header, component. Add storybook

---
+++ src/admin/components/category/category.vue
@@ -0,0 +1,45 @@
+<template>
+  <card slim>
+    <edit-line slot="title" v-model="title" editModeByDefault/>
+    <ul class="skills" slot="content">
+      <li class="item" v-for="skill in skills" :key="skill.id">
+        <skill :skill="skill" />
+      </li>
+    </ul>
+  </card>
+</template>
+
+<script>
+import card from "../card";
+import editLine from "../editLine";
+import skill from "../skill";
+
+const skills = [
+  {id: 0, title: "HTML", percent: 80},
+  {id: 1, title: "CSS", percent: 20},
+  {id: 2, title: "Javascript", percent: 50},
+]
+export default {
+  components: {
+    card, 
+    editLine, 
+    skill
+  },
+  data() {
+    return {
+      title: "",
+      skills
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.skills {
+  flex-direction: column;
+}
+
+.item {
+  width: 100%;
+}
+</style>
